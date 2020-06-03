0848b2d7272ede2cd6a605d0c96bb9d1c5d46d29
prasetyo yudho <pyudo28@gmail.com>
2020-06-03 17:04:32 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curiculum-vitae';
+  storedTheme : string = localStorage.getItem('theme-color')
+
+  constructor() {}
+  ngOnInit() {}
+
+  setTheme() {
+    if(this.storedTheme === 'theme-dark') {
+      localStorage.setItem('theme-color', 'theme-light')
+      this.storedTheme = localStorage.getItem('theme-color')
+    }else{
+      localStorage.setItem('theme-color', 'theme-dark')
+      this.storedTheme = localStorage.getItem('theme-color')
+    }
+  }
 }
